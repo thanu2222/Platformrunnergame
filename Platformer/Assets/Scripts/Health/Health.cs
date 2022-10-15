d3ca20420d2f06e1b16332c04006bd69c7d0e0b2
thanu2222 <20404@wc.school.nz>
2022-10-15 14:29:04 +1300

    added Health bar and spikes

---
+++ Platformer/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+    
+            //iframes
+        }
+        else
+        {
+            if (!dead)
+            {
+                
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+}
